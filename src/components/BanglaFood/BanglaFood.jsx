65a8076892e6bcc1bd8374a8ab3eeb9a8da7ef06
Mdkhan015 <156634956+Mdkhan015@users.noreply.github.com>
2024-02-24 04:45:54 +0000

    Update BanglaFood.jsx

--- src/components/BanglaFood/BanglaFood.jsx
+++ src/components/BanglaFood/BanglaFood.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import Navbar from '../Navbar/Navbar';
 import { Footer } from '../../container';
 import Menu from './Menu';
@@ -8,20 +6,22 @@ import Modal from './Modal'; // Import your modal component
 import './BanglaFood.css';
 
 function BanglaFood() {
-  // Initialize state for tracking the active menu and active button
   const [activeMenu, setActiveMenu] = useState('breakfast');
   const [activeButton, setActiveButton] = useState('breakfast');
   const [isModalOpen, setIsModalOpen] = useState(false); // State for modal visibility
+  const [cartContent, setCartContent] = useState({ items: [], totalPrice: 0 });
 
-  // Function to handle menu clicks
   const handleMenuClick = (menu) => {
     setActiveMenu(menu);
     setActiveButton(menu); // Update active button when menu changes
   };
 
-  // Function to toggle modal visibility
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
+  };
+
+  const updateCartContent = (items, totalPrice) => {
+    setCartContent({ items, totalPrice });
   };
 
   // Render buttons
@@ -45,17 +45,23 @@ function BanglaFood() {
       <div className="menu-buttons">
         {/* Render buttons */}
         {renderButtons()}
-        {/* Render Cart Button with icon */}
-        <button onClick={toggleModal}>
-          <FontAwesomeIcon icon={faShoppingCart} /> Your Cart
-        </button>
+        {/* Render Cart Button */}
+        <button onClick={toggleModal}>Cart</button>
       </div>
       {/* Render menu based on activeMenu */}
-      <Menu activeMenu={activeMenu} />
+      <Menu activeMenu={activeMenu} onCartUpdate={updateCartContent} />
       {/* Render Modal */}
       <Modal isOpen={isModalOpen} onClose={toggleModal}>
         {/* Modal Content Goes Here */}
-        <p>This is the cart modal content</p>
+        <h2>Cart</h2>
+        <ul>
+          {cartContent.items.map((item, index) => (
+            <li key={index}>
+              {item.name} - ${item.price}
+            </li>
+          ))}
+        </ul>
+        <p>Total Price: ${cartContent.totalPrice}</p>
       </Modal>
       <Footer />
     </>
